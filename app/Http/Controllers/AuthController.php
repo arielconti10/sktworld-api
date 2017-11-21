0cce3766c5266462767cbb8b38cf3f002c47667d
ariel <arielconti10@gmail.com>
2017-11-21 06:54:58 -0200

    Register user and auth - needs fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
 use JWTAuth;
 use Tymon\JWTAuth\Exceptions\JWTException;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Validator;
+
 
 class AuthController extends Controller
 {
+    use RegistersUsers;
+    use ValidatesRequests;
+
+
+    protected function validator($request)
+    {
+        return Validator::make($request, [
+            'name' => 'required|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+    }
+
+
     public function login(Request $request){
         $credentials = $request->only('email', 'password');
         try{
@@ -26,5 +45,31 @@ class AuthController extends Controller
     public function getAuthUser(Request $request){
         $user = JWTAuth::toUser($request->token);
         return response()->json(['result' => $user]);
+    }
+
+    public function create(Request $request){
+        return User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+    }
+
+    /**
+     * Send the response after the user was authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    protected function sendLoginResponse(Request $request)
+    {
+        $this->clearLoginAttempts($request);
+        $token = (string) $this->guard()->getToken();
+        $expiration = $this->guard()->getPayload()->get('exp');
+        return [
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => $expiration - time(),
+        ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,13 @@ use Illuminate\Http\Request;
 */
 
 Route::group(['middleware' => ['api','cors']], function () {
-    Route::post('auth/login', 'AuthController@login');
+
+    Route::group(['middleware' => 'guest:api'], function(){
+        Route::post('auth/login', 'AuthController@login');
+        Route::post('auth/register', 'AuthController@register');
+    });
+
+
     Route::group(['middleware' => 'jwt-auth'], function () {
         Route::get('user', 'AuthController@getAuthUser');
     });
